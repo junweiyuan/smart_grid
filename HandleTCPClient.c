93fff457a2bb1305bff6a38dbcc2e31748d5d5f7
Junwei Yuan <junweiyuan2009@gmail.com>
2012-05-02 20:22:23 -0700

    second commit

---
+++ HandleTCPClient.c
@@ -0,0 +1,42 @@
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for recv() and send() */
+#include <unistd.h>     /* for close() */
+#include <string.h>
+
+#define RCVBUFSIZE 32   /* Size of receive buffer */
+
+void DieWithError(char *errorMessage);  /* Error handling function */
+
+void HandleTCPClient(int clntSocket)
+{
+    char echoBuffer[RCVBUFSIZE];        /* Buffer for echo string */
+    int recvMsgSize;                    /* Size of received message */
+
+    /* Receive message from client */
+    if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
+        DieWithError("recv() failed");
+    char *sp=NULL;
+    sp=strtok(echoBuffer," ");
+    while(sp!=NULL)
+	{
+	   printf("result is%s\n",sp);
+	   if(!strcmp(sp,"Get"))
+	   {
+		printf("He is asking Power\n");
+	   }
+	   sp=strtok(NULL," ");
+	 }
+    /* Send received string and receive again until end of transmission */
+    while (recvMsgSize > 0)      /* zero indicates end of transmission */
+    {
+        /* Echo message back to client */
+        if (send(clntSocket, echoBuffer, recvMsgSize, 0) != recvMsgSize)
+            DieWithError("send() failed");
+
+        /* See if there is more data to receive */
+        if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
+            DieWithError("recv() failed");
+    }
+
+    close(clntSocket);    /* Close client socket */
+}
